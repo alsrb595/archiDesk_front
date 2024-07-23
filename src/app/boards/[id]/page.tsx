97d67b268ffa272bbfc59ba2e3f502d64189c154
alsrb595 <alsrb595@gmail.com>
2024-07-23 21:50:15 +0900

    양세희 받아라

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const BoardDetail = () => {
   const postId = useParams();
@@ -11,21 +13,23 @@ const BoardDetail = () => {
   const [postData, setPostData] = useState<any>([]);
   const [modal, setModal] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
+  const [comment, setComment] = useState("");
+  const [commentList, setCommentList] = useState([]);
+
+  const MySwal = withReactContent(Swal);
 
   const fetchBoardData = async () => {
     try {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/boards/${postId.id}`
       );
+      console.log("??", response);
       setPostData(response.data);
+      setCommentList(response.data.comments);
     } catch (error) {
       console.error("에러", error);
     }
   };
-
-  useEffect(() => {
-    fetchBoardData();
-  }, []);
 
   const onClickBackImg = () => {
     setModal((prev) => !prev);
@@ -34,6 +38,55 @@ const BoardDetail = () => {
   const toggleSidebar = () => {
     setOpenSidebar(!openSidebar);
   };
+
+  const onChangeCommnet = (e: any) => {
+    setComment(e.target.value);
+  };
+
+  const onClickCommentSubmit = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/boards/comment/upload`,
+        {
+          postId: Number(postId.id),
+          writer: localStorage.getItem("user"),
+          content: comment,
+        }
+      );
+      MySwal.fire({
+        icon: "success",
+        title: "Complete!",
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1000,
+        customClass: {
+          popup: "swal-custom-popup",
+          title: "swal-custom-title",
+          htmlContainer: "swal-custom-html-container",
+        },
+      });
+    } catch (error) {
+      console.error("comment 등록 실패", error);
+      MySwal.fire({
+        icon: "error",
+        title: "Fail!",
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1000,
+        customClass: {
+          popup: "swal-custom-popup",
+          title: "swal-custom-title",
+          htmlContainer: "swal-custom-html-container",
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchBoardData();
+  }, [onClickCommentSubmit]);
 
   return (
     <BoardDetailWrapper>
@@ -90,19 +143,23 @@ const BoardDetail = () => {
             <RightBottomHeader>Comment List</RightBottomHeader>
             <RightBottomBody>
               <CommentList>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
-                <ShowComments>Comment</ShowComments>
+                {commentList.map((comment, index) => (
+                  <ShowComments key={index}>{comment}</ShowComments>
+                ))}
               </CommentList>
-              <CommentInput placeholder="Enter comment" />
             </RightBottomBody>
+            <RightBottomFooter>
+              <CommentInputWrapper>
+                <CommentInput
+                  placeholder="Enter comment"
+                  onChange={onChangeCommnet}
+                />
+                <CommentSubmit
+                  src="/assets/send.png"
+                  onClick={onClickCommentSubmit}
+                />
+              </CommentInputWrapper>
+            </RightBottomFooter>
           </RightBottom>
         </RightWrapper>
       </DetailBody>
@@ -323,6 +380,7 @@ const RightBottom = styled.div`
 
 const RightBottomHeader = styled.div`
   width: 100%;
+  height: 20%;
   padding: 20px 30px;
   box-sizing: border-box;
   background-color: #4c4c4b;
@@ -334,13 +392,13 @@ const RightBottomBody = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
+  height: 60%;
   font-size: 18px;
 `;
 
 const CommentList = styled.div`
   width: 100%;
-  height: 230px;
+  height: 100%;
   color: #4c4c4b;
   padding: 30px;
   box-sizing: border-box;
@@ -354,14 +412,30 @@ const ShowComments = styled.div`
   color: #4c4c4b;
 `;
 
+const RightBottomFooter = styled.div`
+  width: 100%;
+  height: 20%;
+`;
+
+const CommentInputWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #b4c8bb;
+  padding-right: 20px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+`;
+
 const CommentInput = styled.input`
   width: 100%;
-  background-color: #b4c8bb;
   border: none;
   color: #4c4c4b;
   font-size: 18px;
   padding: 10px 30px;
   box-sizing: border-box;
+  background-color: #b4c8bb;
   border-bottom-left-radius: 15px;
   border-bottom-right-radius: 15px;
 
@@ -369,4 +443,9 @@ const CommentInput = styled.input`
     font-size: 18px;
     color: #4c4c4b;
   }
+`;
+
+const CommentSubmit = styled.img`
+  width: 30px;
+  height: 30px;
 `;
